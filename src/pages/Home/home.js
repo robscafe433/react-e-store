9fd14a67943909a78304c0bdfc54a0332777dc43
Robert S <robscafe333@yahoo.com>
2021-07-26 23:11:59 -0700

    copying all files from master to main, since main branch is now the default branch.

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,12 +1,15 @@
-import './home.css';
+import "./home.css";
 
+
+import Banner from "../../components/Banner/banner";
 
 function Home() {
-  return (
-    <div>
-      <h1 className="hola">Hola Mundo</h1>
-    </div>
-  );
+    return (
+        <div>
+            <Banner />
+            <h1 className="hola">Hola Mundo</h1>
+        </div>
+    );
 }
 
 export default Home;
