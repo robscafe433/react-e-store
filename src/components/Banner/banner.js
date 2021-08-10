0aaf6c0f941c13abfad7bcd4041705090739e989
VelazquezLuis <javiervelazquez113@yahoo.com>
2021-08-09 18:56:04 -0700

    updated class to className

--- src/components/Banner/banner.js
+++ src/components/Banner/banner.js
@@ -3,13 +3,13 @@ import "./banner.css";
 function Banner() {
     /*-- XL- 1450 * 720 lg- 950 * 350 md- 992 * 576 sm- 768 * 576 --*/
     return (
-        <div class="banner">
-            <div class="banner-textWrapper">
-                <h1 class="banner-title">Your one stop shop</h1>
+        <div className="banner">
+            <div className="banner-textWrapper">
+                <h1 className="banner-title">Your one stop shop</h1>
                 <h2>At a discount price</h2>
-                <a href="/categories/all.html" class="btn btn-dark">
+                <a href="/categories/all.html" className="btn btn-dark">
                     View All Items
-                    <i class="fal fa-chevron-right"></i>
+                    <i className="fal fa-chevron-right"></i>
                 </a>
             </div>
         </div>
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -2,71 +2,69 @@ import React from "react";
 
 const Navbar = (props) => {
     return (
-        <div>
-            <header>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light rounded fixed-top">
-                  {/* href needs react setup*/}
-                    <a class="navbar-brand" href="index.html">
-                        Robert's One Stop Shop
-                    </a>
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light rounded fixed-top">
+                {/* href needs react setup*/}
+                <a className="navbar-brand" href="index.html">
+                    Robert's One Stop Shop
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarsExample09"
+                    aria-controls="navbarsExample09"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarsExample09">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            {/* href needs react setup*/}
+                            <a className="nav-link" href="index.html">
+                                Home <span className="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            {/* // onclicks href needs react setup*/}
+                            <a className="nav-link" href="#">
+                                Groceries
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            {/* // onclicks href needs react setup*/}
+                            <a className="nav-link" href="#">
+                                Household
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            {/* // onclicks href needs react setup*/}
+                            <a className="nav-link" href="#">
+                                Garden
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            {/* // onclicks href needs react setup*/}
+                            <a className="nav-link" href="#">
+                                Cart
+                            </a>
+                        </li>
+                    </ul>
+
                     <button
-                        class="navbar-toggler"
-                        type="button"
-                        data-toggle="collapse"
-                        data-target="#navbarsExample09"
-                        aria-controls="navbarsExample09"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarsExample09">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                              {/* href needs react setup*/}
-                                <a class="nav-link" href="index.html">
-                                    Home <span class="sr-only">(current)</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                              {/* // onclicks href needs react setup*/}
-                                <a class="nav-link" href="#">
-                                    Groceries
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                              {/* // onclicks href needs react setup*/}
-                                <a class="nav-link" href="#">
-                                    Household
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                              {/* // onclicks href needs react setup*/}
-                                <a class="nav-link" href="#">
-                                    Garden
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                              {/* // onclicks href needs react setup*/}
-                                <a class="nav-link" href="#">
-                                    Cart
-                                </a>
-                            </li>
-                        </ul>
-
-                        <button
                         // onclicks href needs react setup
-                            onclick="document.location.href='#'" 
-                            class="cart btn btn-outline-success my-2 my-sm-0"
-                            type="submit"
-                        >
-                            Cart <span>0</span>
-                        </button>
-                    </div>
-                </nav>
-            </header>
-        </div>
+                        // onclick="document.location.href='#'"
+                        className="cart btn btn-outline-success my-2 my-sm-0"
+                        type="submit"
+                    >
+                        Cart <span>0</span>
+                    </button>
+                </div>
+            </nav>
+        </header>
     );
 };
 
