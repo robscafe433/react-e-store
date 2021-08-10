63e7489bd7518e0a41d9a3bd1b5038518ec3680d
Felipe Fernandez <heyitsfelipe@gmail.com>
2021-08-09 19:12:06 -0700

    Adds footer.

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,20 @@
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p className="footer-content">© 2020 Robert's One Stop Shop</p>
+            {/* <button
+                type="button"
+                class="btn btn-dark"
+                onclick="topFunction()"
+                id="scrollup"
+                title="Go to top"
+            >
+                Back to top
+            </button> */}
+        </footer>
+    )
+}
+
+export default Footer;
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -2,12 +2,14 @@ import "./home.css";
 
 
 import Banner from "../../components/Banner/banner";
+import Footer from "../../components/Footer/footer";
 
 function Home() {
     return (
         <div>
             <Banner />
             <h1 className="hola">Hola Mundo</h1>
+            <Footer />
         </div>
     );
 }
