b6d8417f4893a40d46258394cf19cf64505b7f40
Felipe Fernandez <heyitsfelipe@gmail.com>
2021-08-21 19:51:21 -0700

    Updates data and adds helper functions.

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,10 +1,12 @@
 import "./home.css";
 import Navbar from "../../components/Navbar/navbar.js"
-
 import Banner from "../../components/Banner/banner";
 import Footer from "../../components/Footer/footer";
+import useFetch from "../../api/store";
+import {getCategories} from "../../helpers/storeHelpers";
 
 function Home() {
+    console.log(">>>", getCategories(useFetch('http://localhost:8000/items')));
     return (
         <div>
             <Navbar/>
