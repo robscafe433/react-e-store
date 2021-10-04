65ade28ffc4b25e4356a63b1e75ee55a61baad7e
Robert S <robscafe333@yahoo.com>
2021-10-03 22:20:57 -0700

    changed images file path. Now all banner images are under Images-CategoryImages-BannerImages. Also in banner.js I changed Banner function to an arrow Banner function. Also in home.css i removed the h1 css ruleset that was causing a bug. Also in banner.css I changed ruleset names using BEM methodology. Also changed media queries pic path.

--- src/components/Banner/banner.js
+++ src/components/Banner/banner.js
@@ -1,19 +1,22 @@
 import "./banner.css";
+import React from "react";
 
-function Banner() {
+const Banner = () => {
     /*-- XL- 1450 * 720 lg- 950 * 350 md- 992 * 576 sm- 768 * 576 --*/
     return (
-        <div className="banner">
-            <div className="banner-textWrapper">
-                <h1 className="banner-title">Your one stop shop</h1>
-                <h2>At a discount price</h2>
-                <a href="/categories/all.html" className="btn btn-dark">
-                    View All Items
-                    <i className="fal fa-chevron-right"></i>
-                </a>
+        <div className="container">
+            <div className="banner">
+                <div className="banner__textWrapper">
+                    <h1 idName="banner__title">Your one stop shop</h1>
+                    <h2>At a discount price</h2>
+                    <a href="/categories/all.html" class="btn btn-dark">
+                        View All Items
+                        <i className="fal fa-chevron-right"></i>
+                    </a>
+                </div>
             </div>
         </div>
     );
-}
+};
 
 export default Banner;
