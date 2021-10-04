0c8fa66e18f746c007ea7c6bd9d35f41062c3c5b
Robert S <robscafe333@yahoo.com>
2021-10-03 22:44:21 -0700

    changed idName to className in banner.js, it was showing up as warning when inspected website

--- src/components/Banner/banner.js
+++ src/components/Banner/banner.js
@@ -7,9 +7,9 @@ const Banner = () => {
         <div className="container">
             <div className="banner">
                 <div className="banner__textWrapper">
-                    <h1 idName="banner__title">Your one stop shop</h1>
+                    <h1 className="banner__title">Your one stop shop</h1>
                     <h2>At a discount price</h2>
-                    <a href="/categories/all.html" class="btn btn-dark">
+                    <a href="/categories/all.html" className="btn btn-dark">
                         View All Items
                         <i className="fal fa-chevron-right"></i>
                     </a>
