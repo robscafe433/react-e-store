23301536905a618e7815b6433526f2e494d319b9
VelazquezLuis <javiervelazquez113@yahoo.com>
2021-10-21 23:32:34 -0700

    Adding data and storehelpers. debugging needed.

--- src/components/Categories/categories.js
+++ src/components/Categories/categories.js
@@ -1,10 +1,14 @@
 import React from "react";
-import Data from "../../api/db.json";
+// import Data from "../../api/db.json";
 import "./categories.css"
 import Category from "./Category";
+import useFetch from "../../api/store";
+import getCategoriesImages from "../../helpers/storeHelpers"
 
 function Categories() {
-    const categoryList = Data.items.map(item => <Category item={item}/>)
+    let data = useFetch("http://localhost:3000/items");
+    let categoryImages = getCategoriesImages(data);
+    const categoryList = categoryImages.items.map((item, index) => <Category index={index} item={item}/>)
     return (
         <div className="album py-5 bg-light">
             <div className="container">
