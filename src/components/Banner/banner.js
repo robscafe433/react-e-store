9d8b71c489ac8131e29e700f9c17a57b42efdea4
Robert S <robscafe333@yahoo.com>
2021-10-23 10:53:49 -0700

    added image from data

--- src/components/Banner/banner.js
+++ src/components/Banner/banner.js
@@ -1,10 +1,20 @@
 import "./banner.css";
 import React from "react";
+import useFetch from "../../api/store";
+import {
+    getCategories,
+    getCategoriesImages,
+    getBanners,
+} from "../../helpers/storeHelpers";
 
 const Banner = () => {
     /*-- XL- 1450 * 720 lg- 950 * 350 md- 992 * 576 sm- 768 * 576 --*/
+    let bannerImage = getBanners(
+        useFetch("http://localhost:8000/storeResources")
+    );
     return (
         <div className="banner">
+            <img src={bannerImage} className="banner" />
             <div className="banner__textWrapper">
                 <h1 className="banner__title">Your one stop shop</h1>
                 <h2>At a discount price</h2>
