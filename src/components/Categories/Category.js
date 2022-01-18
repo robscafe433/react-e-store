68bfd2194abed6e922fce8d0e4f9ebfcb8701997
luis velazquez <javiervelazquez113@yahoo.com>
2022-01-17 22:20:46 -0800

    Made each category Titlecase

--- src/components/Categories/Category.js
+++ src/components/Categories/Category.js
@@ -1,7 +1,12 @@
 import React from "react";
 
+
+
 const Category = (props) => {
     console.log("propsinchild", props.item[0]);
+    function titleCase(str) {
+    return str.toLowerCase().replace(/\b(\w)/g, s => s.toUpperCase());
+  }
     return (
         <div className="col-12 col-md-6 col-lg-4 px-4 px-md-3">
             {/* <!-- cards 380 * 275 --> */}
@@ -13,7 +18,7 @@ const Category = (props) => {
                 />
                 <div className="image-overlay">
                     <a href="#">
-                        <h3 className="card-hoverText">{props.item[0]}</h3>
+                        <h3 className="card-hoverText">{titleCase(props.item[0])}</h3>
                     </a>
                 </div>
             </div>
