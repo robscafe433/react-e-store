ec0cf0ad70fe7ce9a9cc5926329e620c5d5ccf21
Robert S <robscafe333@yahoo.com>
2022-03-09 14:56:54 -0800

    using prettier formatted BannerImage.js, banner.js, and banncer.css

--- src/components/Banner/BannerImage.js
+++ src/components/Banner/BannerImage.js
@@ -5,29 +5,29 @@ import { getBanners } from "../../helpers/storeHelpers";
 let windowWidthPixels = window.innerWidth;
 
 const BannerImage = () => {
-    let bannerImagePic = getBanners(
-        useFetch("http://localhost:8000/storeResources")
+  let bannerImagePic = getBanners(
+    useFetch("http://localhost:8000/storeResources")
+  );
+  if (windowWidthPixels <= 1000) {
+    console.log("pixels are LESS than 1k");
+    return (
+      <img src={bannerImagePic} className="banner" />
+      // <div>
+      //     {console.log("Inside BannerImage component")}
+      //     {console.log("This is line two")};{bannerImagePic}
+      // </div>
     );
-    if (windowWidthPixels <= 1000) {
-        console.log("pixels are LESS than 1k");
-        return (
-            <img src={bannerImagePic} className="banner" />
-            // <div>
-            //     {console.log("Inside BannerImage component")}
-            //     {console.log("This is line two")};{bannerImagePic}
-            // </div>
-        );
-    } else if (windowWidthPixels > 1000) {
-        console.log("pixels are way MORE than 1k");
-        return (
-            <img src={bannerImagePic} className="banner" />
+  } else if (windowWidthPixels > 1000) {
+    console.log("pixels are way MORE than 1k");
+    return (
+      <img src={bannerImagePic} className="banner" />
 
-            // <div>
-            //     {console.log("Inside BannerImage component")}
-            //     {console.log("This is line two")};{bannerImagePic}
-            // </div>
-        );
-    }
+      // <div>
+      //     {console.log("Inside BannerImage component")}
+      //     {console.log("This is line two")};{bannerImagePic}
+      // </div>
+    );
+  }
 };
 
 export default BannerImage;
--- src/components/Banner/banner.js
+++ src/components/Banner/banner.js
@@ -10,22 +10,22 @@ import BannerImage from "./BannerImage";
 // } from "../../helpers/storeHelpers";
 
 const Banner = (prop) => {
-    /*-- XL- 1450 * 720 lg- 950 * 350 md- 992 * 576 sm- 768 * 576 --*/
+  /*-- XL- 1450 * 720 lg- 950 * 350 md- 992 * 576 sm- 768 * 576 --*/
 
-    return (
-        <div className="banner__FirstDiv">
-            <BannerImage />
+  return (
+    <div className="banner__FirstDiv">
+      <BannerImage />
 
-            <div className="banner__textWrapper">
-                <h1 className="banner__title">Your one stop shop</h1>
-                <h2>At a discount price</h2>
-                <a href="/categories/all.html" className="btn btn-dark">
-                    View All Items
-                    <i className="fal fa-chevron-right"></i>
-                </a>
-            </div>
-        </div>
-    );
+      <div className="banner__textWrapper">
+        <h1 className="banner__title">Your one stop shop</h1>
+        <h2>At a discount price</h2>
+        <a href="/categories/all.html" className="btn btn-dark">
+          View All Items
+          <i className="fal fa-chevron-right"></i>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
