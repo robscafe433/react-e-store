551079004c98e19201af4eb6a538409e0e73b4f9
Robert S <robscafe333@yahoo.com>
2022-06-07 13:50:23 -0700

    added col12 on line 17 so that i could subdivide columns underneath that div , also added more items to display on the cart page. for ShoppingCartDescription.js

--- src/components/Cart/ShoppingCartDescription.js
+++ src/components/Cart/ShoppingCartDescription.js
@@ -4,7 +4,7 @@ import "./ShoppingCartDescription.css";
 const ShoppingCartDescription = (props) => {
   const { cartItems } = props;
   return (
-    <div className="col-12 col-md-7 col-lg-8 white">
+    <div className="col-lg-8 white">
       <div className="row mx-2 my-2 p-3 pb-9 border-bottom border-secondary">
         <div className="col-12 col-md-12 col-lg-12 align-self-center">
           <h2>Shopping cart</h2>
@@ -14,13 +14,18 @@ const ShoppingCartDescription = (props) => {
         </div>
       </div>
       <div className="row mx-2 pt-4 border-bott cart-Display">
-        <div className="align-self-center">
+        <div className=" align-self-center col-12 col-md-12 col-lg-12">
           {cartItems.length === 0 && <div>Cart is Empty</div>}
           {cartItems.map((x) => (
-            <div key={x.id}>
-              <img src={x.image} height=" 40"></img>
-              {x.name}
-              {x.price}
+            <div key={x.id} className="row">
+              <div className="col-3">
+                {" "}
+                <img src={x.image} max-width="2px" height="20"></img>
+              </div>
+
+              <div className="col-3">{x.name}</div>
+              <div className="col-3">$ {x.price.toFixed(2)}</div>
+              <div className="col-3">{x.inCart}</div>
             </div>
           ))}
         </div>
