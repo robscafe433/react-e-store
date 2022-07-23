d20b71435ca242bb09c740013f47a3d7bf4020f7
Felipe Fernandez <heyitsfelipe@gmail.com>
2022-07-23 10:49:25 -0700

    fix: fix prettier linter errors
    Fixes prettier linter errors that happened during pull request.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import useFetch from "./api/store";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from './pages/Home/home';
-import Checkout from './pages/Checkout/checkout';
-import ThankYou from './pages/ThankYou/thankyou';
+import Home from "./pages/Home/home";
+import Checkout from "./pages/Checkout/checkout";
+import ThankYou from "./pages/ThankYou/thankyou";
 
 function App() {
   const storeData = useFetch("http://localhost:8000/items");
   return (
     <Router>
       <Routes>
-        <Route path="/react-e-store" exact element={<Home storeData={storeData} />} />
-        <Route path="/react-e-store/checkout"  element={<Checkout />} />
-        <Route path="/react-e-store/thankyou"  element={<ThankYou />} />
+        <Route
+          path="/react-e-store"
+          exact
+          element={<Home storeData={storeData} />}
+        />
+        <Route path="/react-e-store/checkout" element={<Checkout />} />
+        <Route path="/react-e-store/thankyou" element={<ThankYou />} />
       </Routes>
     </Router>
   );
